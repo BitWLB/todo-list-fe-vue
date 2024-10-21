de49d8ddaf297f23fffb6aebd8103494809f3d1a
Willi Buli <buliwilli17@gmail.com>
2024-10-21 21:24:24 +0700

    feat: add dialog and card

---
+++ src/core/components/ui/card/Card.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/core/plugins/shadcnPlugins'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+</script>
+
+<template>
+  <div
+    :class="
+      cn(
+        'rounded-base shadow-light dark:shadow-dark border-2 border-border dark:border-darkBorder bg-main text-black',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </div>
+</template>
---
+++ src/core/components/ui/card/CardDescription.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/core/plugins/shadcnPlugins'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+</script>
+
+<template>
+  <p :class="cn('text-sm text-black font-base !mt-3', props.class)">
+    <slot />
+  </p>
+</template>
