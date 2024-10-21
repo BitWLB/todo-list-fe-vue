b7e8480ba549f5284ad487747d1b12fbd274647b
Willi Buli <buliwilli17@gmail.com>
2024-10-21 21:35:29 +0700

    feat: adjust dialog scroll content

--- src/core/components/ui/dialog/DialogScrollContent.vue
+++ src/core/components/ui/dialog/DialogScrollContent.vue
@@ -34,7 +34,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
       <DialogContent
         :class="
           cn(
-            'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 rounded-base border-2 border-border dark:border-darkBorder bg-white dark:bg-darkBg p-6 shadow-light dark:shadow-dark duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
+            'relative z-50 grid w-full max-w-lg my-8 gap-4 rounded-base border-2 border-border dark:border-darkBorder bg-white dark:bg-darkBg p-6 shadow-light dark:shadow-dark duration-200 md:w-full',
             props.class,
           )
         "
