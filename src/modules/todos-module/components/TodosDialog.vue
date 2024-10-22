36f6c364670a922d7d0ee658d8db0243a435670e
willi buli <buliwilli17@gmail.com>
2024-10-22 12:15:24 +0700

    feat: add popover, calendar, select

--- src/modules/todos-module/components/TodosDialog.vue
+++ src/modules/todos-module/components/TodosDialog.vue
@@ -10,6 +10,8 @@ import {
 } from '@/core/components/ui/dialog'
 import { Button } from '@/core/components/ui/button'
 import { Icon } from '@iconify/vue'
+import { Label } from '@/core/components/ui/label'
+import { Input } from '@/core/components/ui/input'
 </script>
 
 <template>
@@ -30,6 +32,17 @@ import { Icon } from '@iconify/vue'
           Add your todo list here. Click save when you're done.
         </DialogDescription>
       </DialogHeader>
+
+      <div class="grid gap-4 py-4">
+        <div class="grid grid-cols-4 items-center gap-4">
+          <Label for="name" class="text-right"> Name </Label>
+          <Input id="name" class="col-span-3" />
+        </div>
+        <div class="grid grid-cols-4 items-center gap-4">
+          <Label for="username" class="text-right"> Username </Label>
+          <Input id="username" class="col-span-3" />
+        </div>
+      </div>
 
       <DialogFooter>
         <Button type="submit"> Save </Button>
